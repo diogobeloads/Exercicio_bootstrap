eac43e9d4299a00900fdc69650b73c837c0644dd
Diogo Belo <diogobelodss22@gmail.com>
2024-03-04 20:46:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  $(".carousel").carousel({
+    interval: 1000, // Tempo em milissegundos entre os slides (ajuste conforme necessário)
+    pause: "hover", // Pausa a reprodução do carrossel ao passar o mouse sobre ele
+  });
+});
